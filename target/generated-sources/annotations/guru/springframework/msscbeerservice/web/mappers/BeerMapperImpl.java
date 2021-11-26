f698277a3b39442a65439d603b8ccae05a56b4d8
APurice <adrian.purrice@orange.com>
2021-11-26 18:59:12 +0200

    adding ehcache for list beer and get beer by id. closes #20

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-11-26T17:59:38+0200",
+    date = "2021-11-26T18:50:06+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.10 (Oracle Corporation)"
 )
 @Component
